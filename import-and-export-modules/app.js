29d4b6a00837c8982a2ac2a19ae0c967ee504fa9
anwarmontasir <anwarmontasir@gmail.com>
2021-07-21 14:39:08 -0700

    create module for error handlers

--- import-and-export-modules/app.js
+++ import-and-export-modules/app.js
@@ -3,8 +3,14 @@ const express = require('express');
 const app = express();
 
 const mainRoutes = require('./routes');
+const errorHandlers = require('./errorHandlers');
 
+// pass route handlers to app
 app.use(mainRoutes);
+
+// pass 404 and global error handlers
+app.use(errorHandlers.handle404);
+app.use(errorHandlers.handleGlobalError);
 
 /*
 * Helpers for Various Tasks
@@ -17,39 +23,6 @@ const reverseString = (string) => [...string].reverse().join('');
 const shortenString = (string) => {
   return string.length > 50 ? string.substring(0, 50) + "..." : string;
 }
-
-/*
-* 404 and Global Error Handlers
-*/
-
-// Error handler for handling non-existent routes
-app.use((req, res, next) => {
-  // Log statement to indicate that this function is running 
-  console.log('Handling 404 error');
-
-  // Create new error to handle non-existent routes
-  const err = new Error('err');
-  err.status = 404;
-  err.message = 'Oops, page not found. Looks like that route does not exist.';
-
-  // Pass error to global error handler below
-  next(err);
-});
-
-// Global error handler
-app.use((err, req, res, next) => {
-  // Log statement to indicate that this function is running
-  console.log('Handling a global error');
-  console.log(err);
-  
-  // Set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // Set error status and send error message to the page 
-  res.status(err.status || 500);
-  res.send(err.message);
-});
 
 // Turn on Express server
 app.listen(3000, () => {

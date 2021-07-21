d4fec1487bb5d18935657c55f65a39dc0aa59055
anwarmontasir <anwarmontasir@gmail.com>
2021-07-21 14:29:23 -0700

    create module for route handlers

--- import-and-export-modules/app.js
+++ import-and-export-modules/app.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 
+const mainRoutes = require('./routes');
+
+app.use(mainRoutes);
+
 /*
 * Helpers for Various Tasks
 */
@@ -13,39 +17,6 @@ const reverseString = (string) => [...string].reverse().join('');
 const shortenString = (string) => {
   return string.length > 50 ? string.substring(0, 50) + "..." : string;
 }
-
-/*
-* Route Handlers
-*/
-
-// Home route
-app.get('/', (req, res) => {
-  // Log statement to indicate that this function is running
-  console.log('Handling request to root or "home" route, "/"');
-
-  // Create greeting and use helper functions to reverse and shorten a string
-  const greeting = 'Hello World!'
-  const reversedGreeting = reverseString(greeting);
-  const shortenedDesc = shortenString('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel sapien diam. Vestibulum sed turpis id eros varius cursus.');
-
-  // Send greeting to the page
-  res.send(`
-    <h1>${greeting} &#128075;</h1>
-    <p><strong>Reversed greeting:</strong> ${reversedGreeting}</p>
-    <p><strong>Shortened description:</strong> ${shortenedDesc}</p>
-  `);
-});
-
-// Custom error route
-app.get('/error', (req, res) => {
-  // Log statement to indicate that this function is running
-  console.log('Handling request to custom "error" route, "/error"');
-
-  // Create custom error and print error message to the page
-  const err = new Error('err');
-  err.message = 'Oops, it looks like an error occurred.';
-  throw err;
-});
 
 /*
 * 404 and Global Error Handlers
